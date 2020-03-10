4a4602cfdc32cfdd9a1663da8d8bdc80f1e4b385
Yueying Wang <wangyy395@gmail.com>
2020-03-10 18:21:13 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School 
+    attr_accessor :roster
+
+    def initialize(new_name)
+        @roster = {}
+    end
+
+    def add_student(new_name, new_grade)
+        @roster[new_grade] ||= []
+        @roster[new_grade] << new_name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_hash = {}
+        @roster.each do |grade, student|
+            sorted_hash[grade] = student.sort
+        end
+        sorted_hash
+    end
+    
+end
